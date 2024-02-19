cc041fc78f7d858c2adbb2a6abaec8185391d382
tuan0342 <anhphan0110@gmail.com>
2024-02-19 10:30:58 +0700

    upload file to cloudinary

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 const morgan = require("morgan");
 const formidable = require("formidable");
+const { v2: cloudinary } = require("cloudinary");
 const authorRoute = require("./routes/author");
 const bookRouter = require("./routes/book");
 const uploadFile = require("./middlewares/uploadFile");
@@ -34,7 +35,41 @@ app.use(errorHandler);
 app.use("/v1/author", authorRoute);
 app.use("/v1/book", bookRouter);
 
-app.post("/upload-file", uploadFile);
+app.post("/upload-file-to-cloud", uploadFile, async (req, res) => {
+  try {
+    const { files } = req;
+    const myFile = files.profileImage;
+
+    if (Array.isArray(myFile)) {
+      // multiple file upload
+      const fileUploadPromise = myFile.map(async (file) => {
+        const cloudRes = await cloudinary.uploader.upload(file.filepath);
+        console.log(cloudRes);
+      });
+      await Promise.all(fileUploadPromise);
+      res.json({ ok: true });
+    } else {
+      const cloudRes = await cloudinary.uploader.upload(myFile.filepath, {
+        // resource_type: "video",
+        resource_type: "image",
+      });
+      res.json({ ...cloudRes });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/:publicId/delete-file-from-cloud", async (req, res) => {
+  try {
+    const publicId = req.params.publicId;
+
+    const cloudRes = await cloudinary.uploader.destroy(publicId);
+    res.json({ ...cloudRes });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(8000, () => {
   console.log("Server running on port 8000");
--- middlewares/uploadFile.js
+++ middlewares/uploadFile.js
@@ -1,5 +1,23 @@
 const formidable = require("formidable");
 const path = require("path");
+const { CLOUD_SECRET, CLOUD_NAME, CLOUD_KEY } = require("../cloud-config");
+const { v2: cloudinary } = require("cloudinary");
+
+cloudinary.config({
+  api_secret: CLOUD_SECRET,
+  api_key: CLOUD_KEY,
+  cloud_name: CLOUD_NAME,
+});
+
+// ---- fix error type file (ts)
+// import { File } from "formidable"
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       files: { [key: string]: File | File[] }
+//     }
+//   }
+// }
 
 const uploadFile = async (req, res, next) => {
   const form = new formidable.IncomingForm({
@@ -23,11 +41,33 @@ const uploadFile = async (req, res, next) => {
   // });
 
   try {
-    let data = await form.parse(req);
-    console.log(">>> ", data);
-    if (data) {
-      res.json({ ok: true });
+    let [fields, files] = await form.parse(req);
+
+    if (!req.body) req.body = {};
+
+    for (let key in fields) {
+      const value = fields[key];
+      if (value) req.body[key] = value[0];
     }
+
+    if (!req.files) req.files = {};
+
+    for (let key in files) {
+      const value = files[key];
+      if (value) {
+        if (value.length > 1) {
+          req.files[key] = value;
+        } else {
+          req.files[key] = value[0];
+        }
+      }
+    }
+
+    next();
+
+    // if (data) {
+    //   res.json({ ok: true });
+    // }
   } catch (error) {
     next(error);
     return;
